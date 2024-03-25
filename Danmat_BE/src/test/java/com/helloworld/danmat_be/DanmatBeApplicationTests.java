f819b8d0f8bb199390273221fc149d0e49cdc4e9
unanp2 <unanp2@gmail.com>
2024-03-25 17:15:42 +0900

    리엑트와 스프링부트 연동 테스트

---
+++ Danmat_BE/src/test/java/com/helloworld/danmat_be/DanmatBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.danmat_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanmatBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
